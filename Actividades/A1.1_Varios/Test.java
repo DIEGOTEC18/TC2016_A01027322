5c055ca63fd8bf6a86e9b39ae7c508bc48b66fb9
DIEGOTEC18 <a01027322@itesm.mx>
2019-01-19 13:31:26 -0600

    Primera actividad.

---
+++ Actividades/A1.1_Varios/Test.java
@@ -0,0 +1,15 @@
+public class Test{
+
+    public static void main(String[] args){
+
+        //new para reservar memoria:
+        persona ariel = new persona();
+
+        //Gives a name to the created person.
+        ariel.setName("ariel");
+
+        System.out.println(ariel.getName());
+
+    }
+
+}
---
+++ Actividades/A1.1_Varios/persona.java
@@ -0,0 +1,19 @@
+public class persona {
+
+    private int age;
+    private String name;
+    private char gender;
+
+    public String getName(){
+
+        return name;
+
+    }
+
+    public void setName(String name){
+
+        //this hace referencia a la clase persona:
+        this.name = name;
+
+    }
+}
