148bfed1873154d12b5b21ea4365f64bc7a5a3ae
DIEGOTEC18 <a01027322@itesm.mx>
2019-01-25 11:03:41 -0600

    A3.1.Point

---
+++ Actividades/A3.1.Puntos/Point.java
@@ -0,0 +1,42 @@
+public class Point {
+
+    private double x;
+    private double y;
+
+    //Se llama automáticamente
+    public Point(){
+
+        System.out.println("Constructor sin parámetros");
+
+    }
+
+    public Point(double x, double y){
+
+        this.x = x;
+        this.y = y;
+
+    }
+
+    public double getX(){
+
+        return x;
+
+    }
+
+    public double getY(){
+
+        return y;
+
+    }
+
+    public double getDistance(Point myPoint){
+
+        double distance = 0.0;
+
+        distance = Math.sqrt(Math.pow(myPoint.getX() - x, 2) + Math.pow(myPoint.getY() - y, 2));
+
+        return distance;
+
+    }
+
+}
